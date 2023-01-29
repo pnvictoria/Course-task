1c2a8bee3f814cd886829808b7045347b25c33af
Viktoriya Ponomarenko <pnvictoria13.work@gmail.com>
2023-01-30 00:12:28 +0200

    Fix module 1.3 - packaging and tests

--- task-1.3/src/test/java/org/example/DivisionCalculatorTest.java
+++ task-1.3/src/test/java/org/example/DivisionCalculatorTest.java
@@ -1,141 +1,56 @@
 package org.example;
 
+import org.example.division.DivisionCalculator;
+import org.example.division.DivisionResult;
+import org.junit.jupiter.api.Test;
+
+import java.util.Deque;
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
 
 public class DivisionCalculatorTest {
-    DivisionCalculator calculator = new DivisionCalculator();
+    public DivisionCalculator divisionCalculator;
 
-    @Test
-    void makeDivisionShouldThrowExceptionWhenDivisorIsZero() {
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
+    public DivisionCalculatorTest() {
+        divisionCalculator = new DivisionCalculator();
     }
 
     @Test
-    void makeDivisionShouldCorrectOutputWhenDividendLessDivisor() {
-        String expected = "5|10\n" +
-                " |-\n" +
-                " |0\n";
-        assertEquals(expected, calculator.divide(5, 10));
+    void getDivisionQueue_getExpectedIntegers_ifInputIsNumbers() {
+        Stack<Integer> expected = new Stack<>();
+        int[] arr = {1, 24, 25, 12, 14, 28, 29, 36, 38, 4};
+        for (int value : arr) {
+            expected.add(value);
+        }
+        Deque<Integer> actual= divisionCalculator.getDivisionQueue(78945, 4, 19736);
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.pop(), actual.pop());
+        }
     }
 
     @Test
-    void makeDivisionShouldCorrectOutputWhenDividendIsZero() {
-        String expected = "0|10\n" +
-                " |-\n" +
-                " |0\n";
-        assertEquals(expected, calculator.divide(0, 10));
+    void divide_getExpectedIntegers_ifInputIsNumbers() {
+        int dividend = 10;
+        int divider = 20;
+        Stack<Integer> expected = new Stack<>();
+        expected.add(10);
+        expected.add(0);
+
+        DivisionResult actual = divisionCalculator.divide(dividend, divider);
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.pop(), actual.getIntermediateResults().pop());
+        }
     }
 
     @Test
-    void makeDivisionShouldCorrectOutputWhenDivisorIsOne() {
-        String expected = "_55|1\n" +
-                " 5 |--\n" +
-                " - |55\n" +
-                " _5\n" +
-                "  5\n" +
-                "  -\n" +
-                "  0\n";
-        assertEquals(expected, calculator.divide(55, 1));
-    }
+    void divide_getExpectedError_ifDividerNumberIsNull() {
+        int dividend = 100;
+        int divider = 0;
 
-    @Test
-    void makeDivisionShouldCorrectOutputWhenResidueIsZero() {
-        String expected = "_125|5\n" +
-                " 10 |--\n" +
-                " -- |25\n" +
-                " _25\n" +
-                "  25\n" +
-                "  --\n" +
-                "   0\n";
-        assertEquals(expected, calculator.divide(125, 5));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDividendIsNegative() {
-        String expected = "_123|5\n" +
-                " 10 |--\n" +
-                " -- |24\n" +
-                " _23\n" +
-                "  20\n" +
-                "  --\n" +
-                "   3\n";
-        assertEquals(expected, calculator.divide(-123, 5));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDivisorIsNegative() {
-        String expected = "_123|5\n" +
-                " 10 |--\n" +
-                " -- |24\n" +
-                " _23\n" +
-                "  20\n" +
-                "  --\n" +
-                "   3\n";
-        assertEquals(expected, calculator.divide(123, -5));
-    }
-    @Test
-    void makeDivisionShouldCorrectOutputWhenResidueIsNotZero() {
-        String expected = "_123|5\n" +
-                " 10 |--\n" +
-                " -- |24\n" +
-                " _23\n" +
-                "  20\n" +
-                "  --\n" +
-                "   3\n";
-        assertEquals(expected, calculator.divide(123, 5));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDivisorEqualsDividend() {
-        String expected = "_123|123\n" +
-                " 123|-\n" +
-                " ---|1\n" +
-                "   0\n";
-        assertEquals(expected, calculator.divide(123, 123));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDividendIsBigNumber() {
-        String expected = "_78945|4\n" +
-                " 4    |-----\n" +
-                " -    |19736\n" +
-                "_38\n" +
-                " 36\n" +
-                " --\n" +
-                " _29\n" +
-                "  28\n" +
-                "  --\n" +
-                "  _14\n" +
-                "   12\n" +
-                "   --\n" +
-                "   _25\n" +
-                "    24\n" +
-                "    --\n" +
-                "     1\n";
-        assertEquals(expected, calculator.divide(78945, 4));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDividendContainsFewZeroDigitsInside() {
-        String expected = "_100023|5\n" +
-                " 10    |-----\n" +
-                " --    |20004\n" +
-                "    _23\n" +
-                "     20\n" +
-                "     --\n" +
-                "      3\n";
-        assertEquals(expected, calculator.divide(100023, 5));
-    }
-
-    @Test
-    void makeDivisionShouldCorrectOutputWhenDividendContainsFewZeroDigitsAtTheEnd() {
-        String expected = "_5000|10\n" +
-                " 50  |---\n" +
-                " --  |500\n" +
-                "    0\n";
-        assertEquals(expected, calculator.divide(5000, 10));
+        Exception exception = assertThrows(NullPointerException.class, () -> divisionCalculator.divide(dividend, divider), "Invalid error message.");
+        assertEquals("Incorrect divider number", exception.getMessage());
     }
 }
