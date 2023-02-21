85500789a7ec31f5bb4979a680f8d96f06af7995
Viktoriya Ponomarenko <pnvictoria13.work@gmail.com>
2023-02-21 22:08:39 +0200

    Task 1.5

--- task-1.5/src/main/java/org/example/App.java
+++ task-1.5/src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.example.loader.RaceFilesDataLoader;
+
+public class App {
+    public static void main(String[] args) {
+        RaceFilesDataLoader reader = new RaceFilesDataLoader();
+        System.out.println(reader.orderRacers());
     }
 }
